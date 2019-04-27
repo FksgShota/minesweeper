4ec73a30d9637ba42fea96008aafe7b101c8b878
FksgShota <fksg.syotaness@gmail.com>
2019-04-27 11:22:17 +0900

    script add countuptimer

---
+++ script.js
@@ -0,0 +1,73 @@
+;`use strict`
+const pauseButton = document.getElementById(`pauseButton`)
+const levelButton = document.getElementById(`levelButton`)
+const timeCounter = document.getElementById(`timeCounter`)
+const minefield = document.getElementById(`minefield`)
+const mineSwTable = document.getElementById(`mineSwTable`)
+const resetButton = document.getElementById(`resetButton`)
+const flagModeButton = document.getElementById(`flagModeButton`)
+const mineNumber = document.getElementById(`mineNumber`)
+const flagCounter = document.getElementById(`flagCounter`)
+const tdHtmlCollection = document.getElementsByTagName(`td`)
+
+const countUpTimer = {
+  startTime: 0,
+  playTime: 0,
+  elapsedTime: 0,
+  gameClearTime: 0,
+  timerID: ``,
+
+  start() {
+    this.startTime = new Date().getTime()
+    this.startTime -= this.elapsedTime
+    pauseButton.value = `start`
+    pauseButton.textContent = `再開`
+    this.timerID = setInterval(() => {
+      this.outputCounter()
+    }, 1000)
+  },
+
+  outputDisplay() {
+    this.playTime = new Date().getTime()
+    timeCounter.textContent = this.outputFormat(this.playTime - this.startTime)
+  },
+
+  pause() {
+    clearInterval(this.timerID)
+    this.elapsedTime = this.playTime - this.startTime
+    pauseButton.value = `start`
+    pauseButton.textContent = `再開`
+  },
+
+  reset() {
+    clearInterval(this.timerID)
+    this.startTime = this.playTime = this.elapsedTime = 0
+    pauseButton.value = `start`
+    pauseButton.textContent = `スタート`
+    timeCounter.textContent = `00:00:00`
+  },
+
+  outputFormat(milliSec) {
+    const sec = milliSec / 1000
+    let ss = sec.toFixed(0)
+    let mm = Math.floor(ss / 60)
+    ss = ss % 60
+    let hh = Math.floor(mm / 60)
+    mm = mm % 60
+    if (ss < 10) {
+      ss = `0${ss}`
+    }
+    if (mm < 10) {
+      mm = `0${mm}`
+    }
+    if (hh < 10) {
+      hh = `0${hh}`
+    }
+    return `${hh}:${mm}:${ss}`
+  },
+
+  setGameClearTime() {
+    this.pause()
+    this.gameClearTime = this.outputFormat(this.elapsedTime)
+  },
+}
